c6c74db823a27eebc64e200bbbae9a5b955c51e6
ka1rex <90115256+ka1rex@users.noreply.github.com>
2021-10-24 23:03:34 +0400

    FileFinder
    c#

---
+++ FileFinder/FileFinder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace FileFinder
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите путь к директории:");
+            string _catalog = Console.ReadLine();
+
+            Console.WriteLine("Введите название файла:");
+            string _file = Console.ReadLine();
+
+            FileFinder(_catalog, _file);
+        }
+        static void FileFinder(string _catalog, string _file)
+        {
+            foreach (var _findFile in Directory.EnumerateFiles( _catalog, _file, SearchOption.AllDirectories))
+            {
+                FileInfo FI;
+
+                try
+                {
+                    FI = new FileInfo(_findFile);
+                    Console.WriteLine("Название файла:\t" + FI.Name + "\n" + "Размер файла:\t" + FI.Length + "\n" + "Путь к файлу:\t" + FI.FullName);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Файл не найден!");
+                }
+            }
+        }
+    }
+}
